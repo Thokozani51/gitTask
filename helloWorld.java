7a81e4307a806807c9f1b7f6b6e00896a2578fd4
Thokozani51 <zandile_ngwenya10@yahoo.com>
2021-03-16 18:11:50 +0200

    changing files

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,18 @@
-
+import java.util.Scanner;
 public class helloWorld {
 
 	public static void main(String[] args) {
-	System.out.println("Git is Awesome");
+	System.out.println("Hello World");
+	{
+
+		//Scanner input = new Scanner(System.in);
+		//String answer = input.nextLine();
+		
+		System.out.println("Enter your input ");
+		Scanner input = new Scanner(System.in);
+		String answer = input.nextLine();
+		
+		
+	}
 }
 }
