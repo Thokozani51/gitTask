87172e674c372e2bfe1c2470aac51fbffdb96760
Thokozani51 <zandile_ngwenya10@yahoo.com>
2021-03-19 15:26:22 +0200

    updating file

--- helloWorld.java
+++ helloWorld.java
@@ -3,15 +3,10 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 	System.out.println("Hello World");
-	{
-
-		//Scanner input = new Scanner(System.in);
-		//String answer = input.nextLine();
-		
+	{		
 		System.out.println("Enter your input ");
 		Scanner input = new Scanner(System.in);
 		String answer = input.nextLine();
-		
 		
 	}
 }
